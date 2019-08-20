8d89b83d12d72083a7735137d1e575ffb371d664
Joseph Venuto <jbvenuto@gmail.com>
2019-08-20 17:39:53 -0400

    Create an example of constructors

--- Constructors and Call back/app.js
+++ Constructors and Call back/app.js
@@ -26,3 +26,30 @@ function logAnswer(num) {
 }
 
 addition(2, 3, logAnswer);
+
+// Constructors
+// Constructors are a way of creating many objects of the same type
+function Movie(title, director, rating) {
+    this.title = title;
+    this.director = director;
+    this.rating = rating;
+}
+
+// To make a new object using the constructor
+var jaws = new Movie("Jaws", "Spielberg", "PG")
+
+// Method is a function that is a part of the object or constructor
+// Methods can be added to an object 
+jaws.newMethod = function() {
+    return this.title + " is rated " + this.rating;
+}
+
+// To add method to a constructor you have to add it to the code in the constructor
+function Movie(title, director, rating) {
+    this.title = title;
+    this.director = director;
+    this.rating = rating;
+    this.about = function () {
+        return this.title + " by director, " + this.director + " is rated " + this.rating;
+    };
+}
