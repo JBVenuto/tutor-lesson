90eb580fd6adbec9d633de8c915d1b0ee811d82d
Joseph Venuto <jbvenuto@gmail.com>
2019-08-20 17:21:33 -0400

    Added simple example of callback functions

--- Constructors and Call back/app.js
+++ Constructors and Call back/app.js
@@ -1,16 +1,28 @@
 // Call back functions
 
-//Call backs are functions is excecuted after another function has finished executing 
-//You have already been using call back functions.
+// Call backs are functions is excecuted after another function has finished executing 
+// You have already been using call back functions.
 // jQuery is uses callback functions
 $("#btn").click(function() {
-    //this is inside the callback function
+    // this is inside the callback function
     console.log("The button was clicked");
 })
 
-//Ajax uses callback functions
+// Ajax uses callback functions
 $.ajax({url: url, method: "GET"})
 .then(function() {
-    //this is inside the callback function
+    // this is inside the callback function
 })
 
+// Simple example of callback function
+function addition(num1, num2, cb) {
+    var answer = num1 + num2;
+
+    cb(answer);
+}
+
+function logAnswer(num) {
+    console.log("The answer is " + num);
+}
+
+addition(2, 3, logAnswer);
