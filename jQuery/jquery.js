745de47ae9ce94d3e2698bdbf2d27920d62cc440
Joseph Venuto <jbvenuto@gmail.com>
2019-08-21 16:14:28 -0400

    Created file for jQuery lessons and added some basics

---
+++ jQuery/jquery.js
@@ -0,0 +1,23 @@
+// Make sure jQuery is linked ahead of the javaScript link
+// jQuery link for html:
+// <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+// use a .ready function so that everything waits for the document to be ready
+// you can't interact with elements that loaded after the javaScript
+$(document).ready(function(){
+    // all of the code will go inside this callback function
+
+    // target element
+
+    // find attribute name
+    // this can be set to a variable
+    $(this).attr("attribute-name")
+
+    // to make an element you can set a variable to an html tag
+    var newElement = $("<div>");
+    // add class and attributes to the tag
+    newElement.addClass("class-name");
+    newElement.attr("attribute-name", "attribute-value");
+
+    // add newElement to the document
+    $("#parent-element").append(newElement);
+})
