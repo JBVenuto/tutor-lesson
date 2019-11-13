04ad5d09b0e2cf0b81bd83bc3b2d6052bb1e0566
Joseph Venuto <jbvenuto@gmail.com>
2019-11-13 15:56:18 -0500

    Added questions to practice javascript

---
+++ JS Practice/app.js
@@ -0,0 +1,48 @@
+// 1.You get an array of numbers, return the sum of all of the positives ones.
+// Note: if there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {}
+
+
+// 2.Complete the method that takes a boolean value and return a "Yes" string for true, 
+// or a "No" string for false.
+function boolToWord(bool){}
+
+// 3.Implement a function that adds two numbers together and returns their sum in binary. 
+// The conversion can be done before, or after the addition.
+// The binary number returned should be a string.
+function addBinary(a,b) {}
+
+// 4.Given two integers a and b, which can be positive or negative, find the sum of all the 
+// numbers between including them too and return it. If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+function getSum( a,b ){}
+
+// 5.Simple, given a string of words, return the length of the shortest word(s).
+// String will never be empty and you do not need to account for different data types.
+function findShort(s){}
+
+// 6.Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, 
+// and the negatives become positives.You can assume that all values are integers. 
+// Do not mutate the input array/list.
+function invert(array) {}
+
+// 7.An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+function isIsogram(str){}
+
+// 8.Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+// If the input array is empty or null, return an empty array.
+function countPositivesSumNegatives(input) {}
+
+// 9.Simple, remove the spaces from the string, then return the resultant string.
+function noSpace(x){}
+
+// 10.Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, 
+// rounded to the smallest value.
+// For example:
+// time = 3 ----> litres = 1
+// time = 6.7---> litres = 3
+// time = 11.8--> litres = 5
+function litres(time) {}
